65fda26215288406d7ce441cee582977ab330fd1
datajav <156545837+datajav@users.noreply.github.com>
2026-02-25 16:16:05 -0500

    Add files via upload

--- notebooks/02_synthetic_data_creation.py
+++ notebooks/02_synthetic_data_creation.py
@@ -1,10 +1,12 @@
 import pandas as pd 
 import numpy as np
 
+# Total number of days to simulate
 n_days=120
 facility_ids = ["01-24", "01-23", "01-20", "01-02", "01-03", "01-21", "01-19", "PRIV01", "PRIV02", "PRIV03", "PRIV04", "PRIV05", "03-18", "04-16", "05-08", "05-17", "06-05", "07-10", "08-01", "PRIV06", "PRIV07", "PRIV08", "13-12", "14-07", "14-11", "01-22"]
 departments = ["ER", "ICU", "Maternity"]
 
+# Define seasonal factors for each month to simulate the impact of flu season on hospital arrivals
 seasonal_factors = {
     "Jan": 1.0, 
     "Feb": 1.3, #30% increase in February due to the flu
@@ -16,6 +18,7 @@ records = []
 
 date_range = pd.date_range(start="2026-01-01", periods=n_days)
 
+# Simulate hospital arrivals with seasonal variation
 for date in date_range:
     month = date.strftime("%b")
     season_factor = seasonal_factors.get(month,1.0)
